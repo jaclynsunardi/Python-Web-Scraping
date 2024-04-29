83944af1aea389632f770c2f451afebbe499ebe9
Jaclyn Sunardi <jaclyn.sunardi@yahoo.ca>
2024-04-28 21:20:03 -0700

    final

---
+++ app.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+page_to_scrape = requests.get("https://quotes.toscrape.com/")
+soup = BeautifulSoup(page_to_scrape.text, "html.parser")
+
+quotes = soup.findAll("span", attrs={"class":"text"})
+authors = soup.findAll("small", attrs={"class":"author"})
+
+for quote, author in zip(quotes, authors):
+    print(quote.text + " - " + author.text)
